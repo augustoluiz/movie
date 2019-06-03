3f3c4e49c67bcc688774fca36926ccd666f4a897
augustoluiz <augustogu7@yahoo.com.br>
2019-06-02 23:30:33 -0300

    Implementação da controladora AdminSalas

--- src/com/movie/control/AdminSalas.java
+++ src/com/movie/control/AdminSalas.java
@@ -1,5 +1,37 @@
 package com.movie.control;
 
-public class AdminSalas {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.movie.dao.SalaDAO;
+import com.movie.dao.exception.DAOException;
+import com.movie.model.Sala;
+
+@Controller
+public class AdminSalas {
+	
+	private SalaDAO salaDAO = new SalaDAO();
+	
+	@RequestMapping(value = {"/admin/salas"})
+	public ModelAndView Filme() {
+		List<Sala> salas = new ArrayList<>();
+		String erro = null;
+		
+		try {
+			salas = salaDAO.consultaSalas();
+		} catch (DAOException e) {
+			e.printStackTrace();
+			erro = "Erro ao exibir as salas";
+		}
+		
+		ModelAndView mv = new ModelAndView("sala", "salas", salas);
+		mv.addObject("erro", erro);
+		
+		return mv;
+	}
+	
 }
